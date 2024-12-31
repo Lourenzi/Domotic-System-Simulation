72713ecd1e5b9d887c357dcc0f8aae5ca42f61fd
Eddicpp <eduardo.pane04@gmail.com>
2024-12-31 16:36:50 +0100

    Update DataStructure.cpp

--- DataStructure.cpp
+++ DataStructure.cpp
@@ -21,5 +21,33 @@ void DataStructure::set(bool status)
         for (int i =0; i<name_id.size(); i++) if (name_id[i] == "") { name_id[i] = name; ID = i; }
         /*inserisco nellárray id_nome il nome del device e mi salvo in ID la posizione*/
     }
-    for (int i = tempo; i< data.size(); i++) data[ID][tempo] = status; /*spento -> status = false, acceso -> status = true*/
+    for (int i = tempo; i< data.size(); i++) data[ID][i] = status; /*spento -> status = false, acceso -> status = true*/
+    if (status)
+    {
+        start = tempo; /*inizzializzzione variabili del DEVICE*/
+        end = (int)data.size()-1; /*inizzializzzione variabili del DEVICE*/
+    }
+    else end = tempo;
+    
+}
+
+void DataStructure::set(int start_device, int stop_device)
+{
+    if (ID == -1) /*gli devo dare uh ID valido*/
+    {
+        for (int i =0; i<name_id.size(); i++) if (name_id[i] == "") { name_id[i] = name; ID = i; }
+        /*inserisco nellárray id_nome il nome del device e mi salvo in ID la posizione*/
+    }
+    for (int i = start_device; i<= stop_device; i++) data[ID][i] = true;
+    start = start_device; /*inizzializzzione variabili del DEVICE*/
+    end = stop_device; /*inizzializzzione variabili del DEVICE*/
+}
+
+void DataStructure::rm(string name)
+{
+    if (!automatic)
+    {
+        for (int i = end; i<data.size(); i++) data[ID][i] = true; /*togliendo il timer */
+        end = (int)data.size()-1;
+    }
 }
