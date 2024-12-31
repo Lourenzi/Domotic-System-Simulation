716797aac85b7fe103d00ef3d9aa67c61770216b
Eddicpp <eduardo.pane04@gmail.com>
2024-12-31 18:01:45 +0100

    Update DataStructure.h

--- DataStructure.h
+++ DataStructure.h
@@ -4,31 +4,22 @@
 //
 //  Created by Eduardo on 31/12/24.
 //
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include "Device.h"
+#include "EntryStructure.h"
 
 using namespace std;
 class DataStructure : public Device
 {
 private:
-    array<string,10> name_id;
-    array<array<bool, 10> /*10 Device*/, 60 /*minuti*/ * 24 /*ore*/> data;
-    int tempo;
-    
+    vector<EntryStructure> eventi;
 public:
-    DataStructure(); /*array con gli id lo riempio piano piano*/
-    
-    void set (bool status) override;
+    DataStructure(); /*viene creato allínizio quando non ci sono eventi ancora eseguiti*/
+    void set (bool status, int time) override;
     void set (int start_device, int stop_device) override;
     void rm (string name) override;
     
-    array<bool, 10> get_Device_On(int moment);               //mi dice in un certo momento chi e acceso (true = accesp, false = spento)
-    bool get_one_device_on(int id, int moment);              //mi dice in un certo momento se un dispositiov particolare e acceso o spento
-    int get_consumo(int moment);                             //mi dice quanto sto consumando in un istante preciso
-    
-    int get_id();                                            //dal nome mi collego al relativo id
-
+    void sort(EntryStructure entry);
 };
