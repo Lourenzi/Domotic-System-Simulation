168e9ad0fcec4e67b3aa5fecbd4c30dd7853728b
Eddicpp <eduardo.pane04@gmail.com>
2025-01-03 00:42:28 +0100

    Update Device.h

--- Device.h
+++ Device.h
@@ -4,29 +4,77 @@
 //
 //  Created by Eduardo on 31/12/24.
 //
+#ifndef DEVICE_H
+#define DEVICE_H
+
 #include <iostream>
 #include <string>
 
+#include <stdexcept>
 using namespace std;
 
 class Device
 {
 public:
-    Device ();
-    Device (string name_device, double consume_device, bool automatic_device)
-    { name = name_device; consume = consume_device; automatic = automatic_device;};
+    Device () { throw invalid_argument("non e possibile creare un oggetto con queste caratteristiche"); };
     
-    virtual void set (bool status) = 0;
-    virtual void set (int start_device, int stop_device) = 0;
-    virtual void rm (string name) = 0;
-    virtual void show (string name) = 0;
+    Device (string name_device, double consume_device)
+    {
+        name = name_device;
+        consume = consume_device;
+        start = -1;
+        end = -1;
+        status = false;
+        
+        bool automatic = false;
+        int durata_ciclo = -1;
+    };
     
-protected:
-    int ID = -1; /*viene assegnato durante DataStructure*/
+    Device (string name_device, double consume_device, int ciclo)
+    {
+        name = name_device;
+        consume = consume_device;
+        start = -1;
+        end = -1;
+        status = false;
+        
+        bool automatic = true;
+        int durata_ciclo = ciclo;
+    }
+    
+    string get_device_name () {return name;};     void modify_device_name(string newName) {name = newName;};
+    double get_device_consume () {return consume;};     void modify_device_consume(double newConsume) {consume = newConsume;};
+    int get_device_start () {return start;};    void modify_device_start(int newStart) {start = newStart;};
+    int get_device_end () {return end;};    void modify_device_end(int newEnd) {end = newEnd;};
+    bool is_device_on () {return status;};      void modify_device_status(bool newStatus) {status = newStatus;};
+    
+private:
     string name; /*viene assegnato con il costruttore (main)*/
     double consume; /*viene assegnato con il costruttore (main)*/
-    bool automatic; /*viene assegnato con il costruttore (main)*/
     int start;
     int end;
-    bool on;
+    bool status;
+    
+    bool automatic;
+    int durata_ciclo;
 };
+
+
+/*class DeviceAutomatico
+{
+public:
+    DeviceAutomatico () {throw invalid_argument("non e possibile creare un oggetto automatico con queste caratteristiche");};
+    DeviceAutomatico (string name_device, double consume_device, int ciclo_device)
+    {
+        name = name_device;
+        consume = consume_device;
+        durata_ciclo = ciclo_device;
+        start = -1;
+        end = -1;
+        status = false;
+    };
+private:
+    int durata_ciclo;
+};*/
+
+#endif
