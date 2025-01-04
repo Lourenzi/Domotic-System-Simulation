a9b3d27221162e4f13f3ce3e703887ce4072a529
Lourenzi <83138457+Lourenzi@users.noreply.github.com>
2025-01-04 10:50:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
+//Created by Lorenzo
+
+
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "Interface.h"
 
 using namespace std;
 
 int main(){
 
-	string command = "";
+	ofstream log_file;
+	log_file.open("eventLog.txt");
 
+	string command = "";
 	while(command != "end"){
 		getline(cin, command);
-		parseCommand(command);
+		parseCommand(command, log_file);
 	}
 
+	log_file.close();
 	return 0;
 }
