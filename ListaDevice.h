75b808dc317a2cab6b7c10714b2a63aa692ff747
Eddicpp <eduardo.pane04@gmail.com>
2025-01-08 22:43:29 +0100

    Update ListaDevice.h

--- ListaDevice.h
+++ ListaDevice.h
@@ -1,38 +1,74 @@
 //
-//  ListaDevice.h
+//  device.h
 //  progetto_finale
 //
-//  Created by Eduardo on 02/01/25.
+//  Created by Eduardo on 31/12/24.
 //
+#ifndef DEVICE_H
+#define DEVICE_H
 
-#ifndef LISTADEVICE_H
-#define LISTADEVICE_H
-
-#include <vector>
 #include <iostream>
+#include <string>
 
-#include "Device.h"
-
+#include <stdexcept>
 using namespace std;
-class ListaDevice
+
+class Device
 {
-private:
-    vector<Device> lista_Device;
 public:
-    ListaDevice()
+    Device () { throw invalid_argument("non e possibile creare un oggetto con queste caratteristiche"); };
+    
+    Device (string name_device, double consume_device)
     {
-        Device impianto_fotovoltaico ("impianto_fotovoltaico", 1.5/*KW*/); lista_Device.push_back(impianto_fotovoltaico);
-        Device lavatrice ("lavatrice", -2/*KW*/, 110/*minuti*/); lista_Device.push_back(lavatrice);
-        Device lavastoviglie ("lavastoviglie", -1.5/*KW*/, 195/*minuti*/); lista_Device.push_back(lavastoviglie);
-        Device termostato ("termostato", -2/*KW*/); lista_Device.push_back(termostato);
-        Device tapparelle_elettriche ("tapparelle_elettriche", -0.3/*KW*/, 1); lista_Device.push_back(tapparelle_elettriche);
-        Device scaldabagno ("scaldabagno", -1/*KW*/); lista_Device.push_back(scaldabagno);
-        Device frigorifero ("frigorifero", -0.4/*KW*/); lista_Device.push_back(frigorifero);
-        Device forno_a_microonde ("forno_a_microonde", -0.8/*KW*/, 2/*minuti*/); lista_Device.push_back(forno_a_microonde);
-        Device asciugatrice ("asciugatrice", -0.5/*KW*/, 60)/*minuti*/; lista_Device.push_back(asciugatrice);
-        Device televisore ("televisore", -0.2/*KW*/, 60/*minuti*/); lista_Device.push_back(televisore);
+        name = name_device;
+        consume = consume_device;
+
+        /*start e end vengono settati automatiamente vuoti*/
+        
+        status = false;
+        
+        automatic = false;
+        durata_ciclo = -1;
+    };
+    
+    Device (string name_device, double consume_device, int ciclo)
+    {
+        name = name_device;
+        consume = consume_device;
+
+        /*start e end vengono settati automatiamente vuoti*/
+        
+        status = false;
+        
+        automatic = true;
+        durata_ciclo = ciclo;
     }
     
-    vector<Device> get_vector() {return lista_Device;};
+    string get_device_name () {return name;};     void modify_device_name(string newName) {name = newName;};
+    double get_device_consume () {return consume;};     void modify_device_consume(double newConsume) {consume = newConsume;};
+    vector<int> get_device_start () {return start;};    void modify_device_start(int newStart, int i) {start[i] = newStart;};
+    vector<int> get_device_end () {return end;};    void modify_device_end(int newEnd, int i) {end[i] = newEnd;};
+    bool is_device_on () {return status;};      void modify_device_status(bool newStatus) {status = newStatus;};
+    
+    bool is_device_automatic() {return automatic;};
+    int get_device_timer() {return durata_ciclo;};
+    
+    /*debug*/void get_schedaTecnica () 
+    {
+        cout << "nome: " << get_device_name() << " start: ";
+        for (int i =0; i<start.size(); i++) cout << start[i] << " ";
+        cout << " end: ";
+        for (int i =0; i<end.size(); i++) cout << end[i] << " ";
+        cout << " automatico: " << is_device_automatic() << " timer " << get_device_timer() << endl;};
+    
+private:
+    string name; /*viene assegnato con il costruttore (main)*/
+    double consume; /*viene assegnato con il costruttore (main)*/
+    vector<int> start; //vettore dove sono salvate tutti i minutaggi in cui si accende
+    vector<int> end; //vettore in cui sono salvati tutti i minutaggi in cui si spegne
+    bool status;
+    
+    bool automatic;
+    int durata_ciclo;
 };
 #endif
