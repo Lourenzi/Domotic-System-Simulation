d30e3c53975a8942e5302494521b4de8a2792e31
Eddicpp <eduardo.pane04@gmail.com>
2025-01-08 22:43:09 +0100

    Update Device.h

--- Device.h
+++ Device.h
@@ -22,59 +22,53 @@ public:
     {
         name = name_device;
         consume = consume_device;
-        start = -1;
-        end = -1;
+
+        /*start e end vengono settati automatiamente vuoti*/
+        
         status = false;
         
-        bool automatic = false;
-        int durata_ciclo = -1;
+        automatic = false;
+        durata_ciclo = -1;
     };
     
     Device (string name_device, double consume_device, int ciclo)
     {
         name = name_device;
         consume = consume_device;
-        start = -1;
-        end = -1;
+
+        /*start e end vengono settati automatiamente vuoti*/
+        
         status = false;
         
-        bool automatic = true;
-        int durata_ciclo = ciclo;
+        automatic = true;
+        durata_ciclo = ciclo;
     }
     
     string get_device_name () {return name;};     void modify_device_name(string newName) {name = newName;};
     double get_device_consume () {return consume;};     void modify_device_consume(double newConsume) {consume = newConsume;};
-    int get_device_start () {return start;};    void modify_device_start(int newStart) {start = newStart;};
-    int get_device_end () {return end;};    void modify_device_end(int newEnd) {end = newEnd;};
+    vector<int> get_device_start () {return start;};    void modify_device_start(int newStart, int i) {start[i] = newStart;};
+    vector<int> get_device_end () {return end;};    void modify_device_end(int newEnd, int i) {end[i] = newEnd;};
     bool is_device_on () {return status;};      void modify_device_status(bool newStatus) {status = newStatus;};
+    
+    bool is_device_automatic() {return automatic;};
+    int get_device_timer() {return durata_ciclo;};
+    
+    /*debug*/void get_schedaTecnica () 
+    {
+        cout << "nome: " << get_device_name() << " start: ";
+        for (int i =0; i<start.size(); i++) cout << start[i] << " ";
+        cout << " end: ";
+        for (int i =0; i<end.size(); i++) cout << end[i] << " ";
+        cout << " automatico: " << is_device_automatic() << " timer " << get_device_timer() << endl;};
     
 private:
     string name; /*viene assegnato con il costruttore (main)*/
     double consume; /*viene assegnato con il costruttore (main)*/
-    int start;
-    int end;
+    vector<int> start; //vettore dove sono salvate tutti i minutaggi in cui si accende
+    vector<int> end; //vettore in cui sono salvati tutti i minutaggi in cui si spegne
     bool status;
     
     bool automatic;
     int durata_ciclo;
 };
-
-
-/*class DeviceAutomatico
-{
-public:
-    DeviceAutomatico () {throw invalid_argument("non e possibile creare un oggetto automatico con queste caratteristiche");};
-    DeviceAutomatico (string name_device, double consume_device, int ciclo_device)
-    {
-        name = name_device;
-        consume = consume_device;
-        durata_ciclo = ciclo_device;
-        start = -1;
-        end = -1;
-        status = false;
-    };
-private:
-    int durata_ciclo;
-};*/
-
 #endif
