06ab6fb8f71941bc0c70c1653d8a73238a05ec4d
Eddicpp <eduardo.pane04@gmail.com>
2025-01-11 20:25:54 +0100

    Update EntryStructure.h

--- EntryStructure.h
+++ EntryStructure.h
@@ -16,20 +16,73 @@ using namespace std;
 class EntryStructure
 {
 private:
+    
     int keyTime;
     string element;
     bool on;
+    int power;
+    
 public:
-    EntryStructure(int time, string name, bool onStatus) {keyTime = time; element = name; on = onStatus;};
+    
+    EntryStructure(int time, string name, bool onStatus, int potenza)
+    {
+        keyTime = time;
+        element = name;
+        on = onStatus;
+        power = potenza;
+    };
+    
     /*creo una tripletta momento evento-id elemtno-nuovo stato elemtno*/
-    int get_keyTime() {return keyTime;};
-    string get_element() {return element;};
-    bool get_status() {return on;};
+    int get_keyTime()
+    {
+        return keyTime;
+    };
+    
+    string get_element()
+    {
+        return element;
+    };
+    
+    bool get_status()
+    {
+        return on;
+    };
+    
+    int get_power()
+    {
+        return power;
+    };
     
     EntryStructure copy (EntryStructure vecchio)
     {
-        EntryStructure nuovo (vecchio.get_keyTime(), vecchio.get_element(), vecchio.get_status());
+        EntryStructure nuovo (vecchio.get_keyTime(), vecchio.get_element(), vecchio.get_status(), vecchio.get_power());
         return nuovo;
+    };
+};
+
+class EntryAccesi
+{
+private:
+    
+    string name;
+    int power;
+    
+public:
+    
+    EntryAccesi(string nome_dispositivo, int potenza)
+    {
+        name = nome_dispositivo;
+        power = potenza;
+    };
+    
+    string get_Name()
+    {
+        return name;
+    };
+    
+    int get_Power()
+    {
+        return power;
     };
 };
 
