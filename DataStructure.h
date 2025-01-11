7406c6cb3ad980a807d5eb51f832047709737aea
Eddicpp <eduardo.pane04@gmail.com>
2025-01-11 19:57:09 +0100

    Update DataStructure.h

--- DataStructure.h
+++ DataStructure.h
@@ -4,6 +4,10 @@
 //
 //  Created by Eduardo on 31/12/24.
 //
+
+#ifndef DATASTRUCTURE_H
+#define DATASTRUCTURE_H
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,8 +20,12 @@ using namespace std;
 class DataStructure
 {
 private:
+    
     vector<EntryStructure> eventi;
+    //vector<string> accensioni;    /*salvo in ordine le accensioni che sono avvenute*/
+    
 public:
+    
     DataStructure(); /*viene creato allínizio quando non ci sono eventi ancora eseguiti*/
     
     void set (Device device, bool status);
@@ -25,7 +33,12 @@ public:
     void set (Device device, int start_device);
     void rm (Device device);
     
+    vector<string> get_device_in_order ();
+    
     void stampa ();
     
     void sort(EntryStructure entry);
+    void elimina(int i);
 };
+
+#endif
