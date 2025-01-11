81f54bb9f56056d9b4b030f410e05e4a0aa56b9a
Eddicpp <eduardo.pane04@gmail.com>
2025-01-12 00:25:04 +0100

    Update Device.h

--- Device.h
+++ Device.h
@@ -4,6 +4,7 @@
 //
 //  Created by Eduardo on 31/12/24.
 //
+
 #ifndef DEVICE_H
 #define DEVICE_H
 
@@ -11,6 +12,7 @@
 #include <string>
 
 #include <stdexcept>
+
 using namespace std;
 
 class Device
@@ -46,29 +48,27 @@ public:
     
     string get_device_name () {return name;};     void modify_device_name(string newName) {name = newName;};
     double get_device_consume () {return consume;};     void modify_device_consume(double newConsume) {consume = newConsume;};
-    vector<int> get_device_start () {return start;};    void modify_device_start(int newStart, int i) {start[i] = newStart;};
-    vector<int> get_device_end () {return end;};    void modify_device_end(int newEnd, int i) {end[i] = newEnd;};
+    
+    int get_device_start () {return start;};    void modify_device_start(int newStart) {start = newStart;};
+    int get_device_end () {return end;};    void modify_device_end(int newEnd) {end = newEnd;};
+    
     bool is_device_on () {return status;};      void modify_device_status(bool newStatus) {status = newStatus;};
     
     bool is_device_automatic() {return automatic;};
     int get_device_timer() {return durata_ciclo;};
     
-    /*debug*/void get_schedaTecnica () 
-    {
-        cout << "nome: " << get_device_name() << " start: ";
-        for (int i =0; i<start.size(); i++) cout << start[i] << " ";
-        cout << " end: ";
-        for (int i =0; i<end.size(); i++) cout << end[i] << " ";
-        cout << " automatico: " << is_device_automatic() << " timer " << get_device_timer() << endl;};
+    /*debug void get_schedaTecnica ()
+    { }; */
     
 private:
     string name; /*viene assegnato con il costruttore (main)*/
     double consume; /*viene assegnato con il costruttore (main)*/
-    vector<int> start; //vettore dove sono salvate tutti i minutaggi in cui si accende
-    vector<int> end; //vettore in cui sono salvati tutti i minutaggi in cui si spegne
+    int start; //vettore dove sono salvate tutti i minutaggi in cui si accende
+    int end; //vettore in cui sono salvati tutti i minutaggi in cui si spegne
     bool status;
     
     bool automatic;
     int durata_ciclo;
 };
+
 #endif
