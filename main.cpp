9710e493c7e0ac1b46ff09a8ac57ef9aee6a3a22
Lourenzi <83138457+Lourenzi@users.noreply.github.com>
2025-01-14 21:49:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 int main(){
 
-	ofstream log_file;
-	log_file.open("eventLog.txt");
 	Interface terminal;
 	string command = "";
+	cout<<"||||| Sistema Domotico |||||\n\n"<<"Scrivere 'end' per terminare il programma\n\n";
+
 	while(command != "end"){
+		cout<<">> ";
 		getline(cin, command);
-		terminal.parseCommand(command, log_file);
+		if(command != "end") { terminal.parseCommand(command); }
 	}
 
-	log_file.close();
 	return 0;
 }
